b5e3a78f0852b9f934b56f7764ab65716d67cbaa
FTeichmann <Franz.Teichmann@gmx.de>
2015-07-01 14:15:42 +0200

    updated inline documentation

--- formhandler.js
+++ formhandler.js
@@ -1,13 +1,17 @@
+//wait for the document to be loaded
 $(document).ready(function(){
   $('#submit').click(function(e){
+    //preventing default reload of the website after submit click
     e.preventDefault();
+    //loading parameters into variables
     var getParams = {
         type: $('#type').val(),
         status: $('#status').val(),
         urgency: $('#urgency').val()
     };
+    //request php answer to get parameters
     $.get('requestHandler.php', getParams, function(data){
-      $('#reply').html(data);
+      $('#reply').html(data);//print html answer
     });
   });
 });
--- requestHandler.php
+++ requestHandler.php
@@ -5,20 +5,23 @@ require('vendor/autoload.php');
 require_once 'config.php'; //loading project credentials
 require_once 'documentHandler.php';
 
+//initializing documentHandler object
 $documentHandler = new documentHandler($client_email,$scopes,$private_key,$privatekey_pass,$grant,$user_to_impersonate);
 
+//building file constraints from GET parameters
 $constraints = array();
 foreach($_GET as $key => $value){
     if($value != 'all')
         $constraints[$key] = $value;     
 }
 
+//searching files that match these constraints
 $result = $documentHandler->searchByDescription($constraints);
 if(!$result){
     echo "<p>Sorry, no Topic matched your criteria.</p>";
 }else{
-
     $table = "<table>\n<tr><th>Titel</th><th>Downloadlink</th></tr>\n";
+    //filling the result html table
     foreach ($result as $x){
         $table .= "<tr><th>".$documentHandler->getTitleById($x)."</th>";
         $table .= "<th><a href=\"".$documentHandler->getDownloadLink($documentHandler->searchById($x))."\">Link</a></th></tr>\n";
