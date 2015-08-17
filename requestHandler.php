c6e4ede37221647563f505d6690a43b263ee1bf6
Natanael Arndt <arndtn@gmail.com>
2015-08-17 12:12:27 +0200

    Add cors function to requestHandler.php

--- requestHandler.php
+++ requestHandler.php
@@ -2,9 +2,46 @@
 
 require_once 'vendor/autoload.php';
 
+/**
+ *  An example CORS-compliant method.  It will allow any GET, POST, or OPTIONS requests from any
+ *  origin.
+ *
+ *  In a production environment, you probably want to be more restrictive, but this gives you
+ *  the general idea of what is involved.  For the nitty-gritty low-down, read:
+ *
+ *  - https://developer.mozilla.org/en/HTTP_access_control
+ *  - http://www.w3.org/TR/cors/
+ *
+ * source: http://stackoverflow.com/a/9866124/414075
+ */
+function cors() {
+
+    // Allow from any origin
+    if (isset($_SERVER['HTTP_ORIGIN'])) {
+        header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+        header('Access-Control-Allow-Credentials: true');
+        header('Access-Control-Max-Age: 86400');    // cache for 1 day
+    }
+
+    // Access-Control headers are received during OPTIONS requests
+    if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+
+        if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+            header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+
+        if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+            header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+
+        exit(0);
+    }
+
+    //echo "You have CORS!";
+}
+
 require_once 'config.php'; //loading project credentials
 require_once 'documentHandler.php';
 
+cors();
 //initializing documentHandler object
 $documentHandler = new documentHandler($client_email,$scopes,$private_key,$privatekey_pass,$grant,$user_to_impersonate);
 
@@ -12,7 +49,7 @@ $documentHandler = new documentHandler($client_email,$scopes,$private_key,$priva
 $constraints = array();
 foreach($_GET as $key => $value){
     if($value != 'all')
-        $constraints[$key] = $value;     
+        $constraints[$key] = $value;
 }
 
 //searching files that match these constraints
