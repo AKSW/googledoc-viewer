7c9219e463f6c2318f7e21d33262b1370cef30c6
FTeichmann <Franz.Teichmann@gmx.de>
2016-05-30 07:14:02 +0200

    improvied documentation on configLoader.php

--- backend/serviceConfigs/configLoader.php
+++ backend/serviceConfigs/configLoader.php
@@ -1,24 +1,18 @@
 <?php
 $configToken = array();
-//read this directory (serviceConfigs)
+//read current directory (serviceConfigs)
 $directoryList = scandir(__DIR__);
 foreach($directoryList as $entry){
+    //filter current directory for every .ini file excluding .ini.tpl files
     if(strpos($entry,".ini") == (strlen($entry) - 4)){
         $ini = parse_ini_file($entry);
         $ini_keys = array_keys($ini);
+        //iterate over every ini entry
         for($i = 0; $i < sizeof($ini_keys); $i++){
+            //replace DIR with actual current directory
+            //for reading with file_get_contents of binary files lateron
             $ini[$ini_keys[$i]] = str_replace("DIR",__DIR__,$ini[$ini_keys[$i]]);
         }
         array_push($configToken,$ini);
     }
 }
-
-
-
-//filter service config files
-
-// load all configs organized in this folder
-
-//parse_ini_file
-
-// gather information
