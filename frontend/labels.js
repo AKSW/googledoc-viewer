3f822d8c645b5c9959ce5a8870e0f9c0e143e660
Natanael Arndt <arndtn@gmail.com>
2021-09-19 20:41:43 +0200

    Add status label

--- frontend/labels.js
+++ frontend/labels.js
@@ -1,3 +1,4 @@
 var labels = {
-    "Sorry, no topic matched your criteria." : "Sorry, no topic matched your criteria."
+    "Sorry, no topic matched your criteria." : "Sorry, no topic matched your criteria.",
+    "status":"Status"
 };
